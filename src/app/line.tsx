770bc61e772fb326c1761892fff70cc7fd13f38d
Reche Soares <rechesoares13@gmail.com>
2025-04-27 13:05:14 +0530

    bug fix

--- src/app/line.tsx
+++ src/app/line.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Line = () => {
   const path = useRef<SVGPathElement | null>(null);
--- src/components/SvgPath/AboutCircle.tsx
+++ src/components/SvgPath/AboutCircle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { motion, useSpring } from "framer-motion";
 import { bebasNeue } from "@/assets/fonts";
 
