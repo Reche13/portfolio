fef6b650ffc10bb804010bfdb425086e42c27722
Reche Soares <rechesoares13@gmail.com>
2025-11-21 12:04:18 +0530

    add hero section technology capsules

---
+++ src/components/common/TechCapsule.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { SVGProps, JSX } from "react";
+
+interface Props {
+  label: string;
+  icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+}
+
+export const TechCapsule = ({ label, icon: Icon }: Props) => {
+  return (
+    <div className="rounded-lg inline-flex translate-y-1 items-center gap-2 bg-zinc-200 dark:bg-zinc-800 border-1 border-dashed border-zinc-400 dark:border-zinc-500 w-fit px-2 py-1 cursor-pointer">
+      <Icon className="size-5" />
+      <span className="text-sm font-medium text-zinc-900 dark:text-zinc-50">
+        {label}
+      </span>
+    </div>
+  );
+};
--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { GolangIcon } from "@/assets/tech-icons/golang";
+import { NextIcon } from "@/assets/tech-icons/next";
+import { PostgreSQLIcon } from "@/assets/tech-icons/postgresql";
+import { ReactIcon } from "@/assets/tech-icons/react";
+import { TypeScriptIcon } from "@/assets/tech-icons/typescript";
 import { Container } from "@/components/common/Container";
+import { TechCapsule } from "../common/TechCapsule";
 
 export const Hero = () => {
   return (
@@ -7,14 +15,18 @@ export const Hero = () => {
         <h1 className="text-3xl font-bold text-zinc-900 dark:text-zinc-50">
           Reché Soares
         </h1>
-        <p className="mt-1 font-medium text-lg text-zinc-700 dark:text-zinc-300">
+        <p className="mt-1 font-medium text-lg leading-loose text-zinc-700 dark:text-zinc-300">
           I'm a{" "}
           <span className="font-semibold text-orange-600">
             Full Stack Developer
           </span>{" "}
-          who loves building interactive web apps using Golang, TypeScript,
-          Postgres, React & Next.js, pairing strong backend engineering with
-          clean, intuitive interfaces.
+          who loves building interactive web apps using{" "}
+          <TechCapsule label="Golang" icon={GolangIcon} />,{" "}
+          <TechCapsule label="TypeScript" icon={TypeScriptIcon} />,{" "}
+          <TechCapsule label="PostgreSQL" icon={PostgreSQLIcon} />,{" "}
+          <TechCapsule label="React" icon={ReactIcon} />, &{" "}
+          <TechCapsule label="Next.js" icon={NextIcon} />, pairing strong
+          backend engineering with clean, intuitive interfaces.
         </p>
       </div>
     </Container>
