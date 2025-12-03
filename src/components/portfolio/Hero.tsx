b46583a157d5f4136ff4fa2120d151b1fcc72a58
Reche Soares <rechesoares13@gmail.com>
2025-12-03 16:36:49 +0530

    update about sec

--- src/components/portfolio/Hero.tsx
+++ src/components/portfolio/Hero.tsx
@@ -15,12 +15,16 @@ export const Hero = () => {
         <h1 className="text-4xl font-bold text-zinc-900 dark:text-zinc-50 selection:text-zinc-50 selection:bg-orange-600">
           Reché Soares
         </h1>
-        <div className="mt-4 font-medium text-lg leading-relaxed text-zinc-600 dark:text-zinc-400 selection:bg-orange-600 selection:text-zinc-50">
+        <div className="mt-4 font-medium text-lg leading-[1.55] tracking-tight text-zinc-600 dark:text-zinc-400 selection:bg-orange-600 selection:text-zinc-50">
           <span>I'm a </span>
           <span className="font-semibold text-orange-600">
             Full Stack Developer
           </span>
-          <span> who loves building interactive web apps using </span>
+          <span>
+            {" "}
+            with <span className="font-semibold">3.5+</span> years of experience
+            building interactive web apps using{" "}
+          </span>
           <TechCapsule
             className="translate-y-1"
             label="Golang"
